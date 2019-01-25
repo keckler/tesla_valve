db0ad7b86afb11c3905a9826fb59742b9bb1a802
Chris Keckler <keckler@berkeley.edu>
2019-01-25 15:45:18 -0800

    got driver to run a single sas restart and store the results in a separate file, sas_cumulative_csv.csv

--- coupled/driver.py
+++ coupled/driver.py
@@ -6,6 +6,15 @@
 ############################################################################
 
 #####
+#user inputs
+#####
+
+nominal_height = 0.05
+max_height = 3.0
+max_reactivity = -1
+dt = 0.01
+
+#####
 #imports
 #####
 
@@ -29,8 +38,9 @@ from sys import argv
 #sam_exec = 'sam-opt'
 #sas_post_processor = '/Users/keckler/Documents/work/codes/mini-5.1/plot/PRIMAR4toCSV-Darwin.x'
 sas_input_restart = ''
-sas_restart = ''
+sas_restart_template = 'restarter_tmp.inp'
 sam_input_template = 'tesla_valve_tmp.i'
+sas_cumulative_csv = 'sas_cumulative_csv.csv'
 
 #####
 #run codes iteratively
@@ -40,16 +50,18 @@ sam_input_template = 'tesla_valve_tmp.i'
 print('running original sas...')
 p = Popen(['./run_sas_original.sh'])
 p.wait()
-print('ran original sas input')
+print('done')
 
-#extract core outlet temp
+#extract core outlet temp and save sas results to new csv file
 p = Popen(['./post_process_sas.sh'])
 p.wait()
 print('post processed')
+fsascsv = open(sas_cumulative_csv, 'a+')
 with open('PRIMAR4.csv') as sas_results:
     for row in sas_results:
-        pass
+        fsascsv.write(row)
 last_line = row
+step = int(last_line.split(',')[0])
 time = float(last_line.split(',')[3])
 outlet_temp = float(last_line.split(',')[5])
 
@@ -80,14 +92,13 @@ fsam.write("x = '0 1'\n")
 fsam.write("y = '783 783'\n")
 fsam.write("[../]\n")
 fsam.write('[]')
-
 fsam.close()
 
 #run sam once
 print('running sam...')
 p = Popen(['./run_sam.sh'])
 p.wait()
-print('ran sam')
+print('done')
 
 #extract liquid column height
 with open('tesla_valve_csv.csv') as sam_results:
@@ -100,12 +111,9 @@ liquid_height = float(last_line.split(',')[-12])
 remove('tesla_valve_out_displaced.e')
 
 #print just finished time step
-print('time = '+str(time))
+print('step = '+str(step)+' time = '+str(time))
 
 #convert liquid height to reactivity
-nominal_height = 0.05
-max_height = 3.0
-max_reactivity = 1
 if liquid_height < nominal_height:
     reactivity = 0.0
 elif liquid_height < max_height:
@@ -113,4 +121,52 @@ elif liquid_height < max_height:
 else:
     reactivity = max_reactivity
 
-print(reactivity)
+print('reactivity = '+str(reactivity))
+
+#open sas restart file
+copy(sas_restart_template, 'restarter.inp')
+fsas = open('restarter.inp', 'a+')
+
+#edit sas restart file
+fsas.write('INPCOM     1     0     1\n')
+fsas.write('    11     1'+str(step+1)+'\n')
+fsas.write('    -1\n')
+fsas.write('POWINA    12     1     1\n')
+fsas.write('    29     2 0.         '+str(reactivity)+'\n')
+fsas.write('    49     2 '+str(time)+'          '+str(time+dt)+'\n')
+fsas.write('    -1\n')
+fsas.write('ENDJOB    -1\n')
+fsas.close()
+
+#run restart
+print('running sas...')
+p = Popen(['./run_sas_restart.sh'])
+p.wait()
+print('done')
+
+#extract core outlet temp and save sas results to new csv file
+p = Popen(['./post_process_sas.sh'])
+p.wait()
+print('post processed')
+with open('PRIMAR4.csv') as sas_results:
+    for row in sas_results:
+        fsascsv.write(row)
+last_line = row
+step = int(last_line.split(',')[0])
+time = float(last_line.split(',')[3])
+outlet_temp = float(last_line.split(',')[5])
+
+#clean up
+try:
+    remove('SAS.log')
+    remove('SAS.pid')
+except (OSError):
+    pass
+remove('PRIMAR4.dat')
+remove('CHANNEL.dat')
+move('RESTART.dat','RESTART.bin')
+
+
+
+
+fsascsv.close()
---
+++ coupled/run_sas_restart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/Users/keckler/Documents/work/codes/mini-5.1/bin/mini-5.1-Darwin.x < restarter.inp > sas.o
